4b73d5ba1f1b675fc9d64af5a428cf0af5dc3a9a
Ivkoto <ikostov87@gmail.com>
2019-08-13 02:55:55 +0300

    Programming fundamentals exercise

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace TurtleGraphics2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonHide = new System.Windows.Forms.Button();
+            this.buttonHexagon = new System.Windows.Forms.Button();
+            this.buttonStar = new System.Windows.Forms.Button();
+            this.buttonSpiral = new System.Windows.Forms.Button();
+            this.buttonSun = new System.Windows.Forms.Button();
+            this.buttonTriangle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(27, 12);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(75, 23);
+            this.buttonDraw.TabIndex = 0;
+            this.buttonDraw.Text = "D R A W";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(27, 41);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 1;
+            this.buttonReset.Text = "R E S E T";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // buttonHide
+            // 
+            this.buttonHide.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonHide.Location = new System.Drawing.Point(27, 70);
+            this.buttonHide.Name = "buttonHide";
+            this.buttonHide.Size = new System.Drawing.Size(75, 23);
+            this.buttonHide.TabIndex = 2;
+            this.buttonHide.Text = "HIDE TURTLE";
+            this.buttonHide.UseVisualStyleBackColor = true;
+            this.buttonHide.Click += new System.EventHandler(this.buttonHide_Click);
+            // 
+            // buttonHexagon
+            // 
+            this.buttonHexagon.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonHexagon.Location = new System.Drawing.Point(27, 99);
+            this.buttonHexagon.Name = "buttonHexagon";
+            this.buttonHexagon.Size = new System.Drawing.Size(75, 23);
+            this.buttonHexagon.TabIndex = 3;
+            this.buttonHexagon.Text = "H E X A G O N";
+            this.buttonHexagon.UseVisualStyleBackColor = true;
+            this.buttonHexagon.Click += new System.EventHandler(this.buttonHexagon_Click);
+            // 
+            // buttonStar
+            // 
+            this.buttonStar.Location = new System.Drawing.Point(27, 128);
+            this.buttonStar.Name = "buttonStar";
+            this.buttonStar.Size = new System.Drawing.Size(75, 23);
+            this.buttonStar.TabIndex = 4;
+            this.buttonStar.Text = "S T A R";
+            this.buttonStar.UseVisualStyleBackColor = true;
+            this.buttonStar.Click += new System.EventHandler(this.buttonStar_Click);
+            // 
+            // buttonSpiral
+            // 
+            this.buttonSpiral.Location = new System.Drawing.Point(27, 157);
+            this.buttonSpiral.Name = "buttonSpiral";
+            this.buttonSpiral.Size = new System.Drawing.Size(75, 23);
+            this.buttonSpiral.TabIndex = 5;
+            this.buttonSpiral.Text = "S P I R A L";
+            this.buttonSpiral.UseVisualStyleBackColor = true;
+            this.buttonSpiral.Click += new System.EventHandler(this.buttonSpiral_Click);
+            // 
+            // buttonSun
+            // 
+            this.buttonSun.Location = new System.Drawing.Point(27, 186);
+            this.buttonSun.Name = "buttonSun";
+            this.buttonSun.Size = new System.Drawing.Size(75, 23);
+            this.buttonSun.TabIndex = 6;
+            this.buttonSun.Text = "S U N";
+            this.buttonSun.UseVisualStyleBackColor = true;
+            this.buttonSun.Click += new System.EventHandler(this.buttonSun_Click);
+            // 
+            // buttonTriangle
+            // 
+            this.buttonTriangle.Location = new System.Drawing.Point(27, 216);
+            this.buttonTriangle.Name = "buttonTriangle";
+            this.buttonTriangle.Size = new System.Drawing.Size(75, 23);
+            this.buttonTriangle.TabIndex = 7;
+            this.buttonTriangle.Text = "TRIANGLE";
+            this.buttonTriangle.UseVisualStyleBackColor = true;
+            this.buttonTriangle.Click += new System.EventHandler(this.buttonTriangle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 350);
+            this.Controls.Add(this.buttonTriangle);
+            this.Controls.Add(this.buttonSun);
+            this.Controls.Add(this.buttonSpiral);
+            this.Controls.Add(this.buttonStar);
+            this.Controls.Add(this.buttonHexagon);
+            this.Controls.Add(this.buttonHide);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonDraw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonHide;
+        private System.Windows.Forms.Button buttonHexagon;
+        private System.Windows.Forms.Button buttonStar;
+        private System.Windows.Forms.Button buttonSpiral;
+        private System.Windows.Forms.Button buttonSun;
+        private System.Windows.Forms.Button buttonTriangle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Nakov.TurtleGraphics;
+
+namespace TurtleGraphics2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+                        
+            Turtle.Delay = 300;
+
+            for (int i = 0; i <= 3; i++)
+            {
+                // Draw a equilateral triangle
+                Turtle.Rotate(30);
+                Turtle.Forward(200);
+                Turtle.Rotate(120);
+                Turtle.Forward(200);
+                Turtle.Rotate(120);
+                Turtle.Forward(200);
+
+                // Draw a line in the triangle
+                Turtle.Rotate(-30);
+                Turtle.PenUp();
+                Turtle.Backward(50);
+                Turtle.PenDown();
+                Turtle.Backward(100);
+                Turtle.PenUp();
+                Turtle.Forward(150);
+                Turtle.PenDown();
+                Turtle.Rotate(30);
+            }            
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            Turtle.Reset();
+        }
+
+        private void buttonHide_Click(object sender, EventArgs e)
+        {
+            if (Turtle.ShowTurtle)
+            {
+                Turtle.ShowTurtle = false;
+                this.buttonHide.Text = "Show Turtle";
+            }
+            else
+            {
+                Turtle.ShowTurtle = true;
+                this.buttonHide.Text = "Hide Turtle";
+            }
+        }
+
+        private void buttonHexagon_Click(object sender, EventArgs e)
+        {
+            Turtle.Delay = 300;
+            for (int i = 0; i < 6; i++)
+            {
+                Turtle.Rotate(60);
+                Turtle.Forward(100);
+            }
+        }
+
+        private void buttonStar_Click(object sender, EventArgs e)
+        {
+            Turtle.Delay = 300;
+            Turtle.PenColor = Color.Green;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Turtle.Forward(200);
+                Turtle.Rotate(144);
+            }
+        }
+
+        private void buttonSpiral_Click(object sender, EventArgs e)
+        {
+            
+            Turtle.Delay = 300;
+            for (int i = 0; i < 20; i++)
+            {
+                Turtle.Forward(i * 10);
+                Turtle.Rotate(60);                
+            }
+        }
+
+        private void buttonSun_Click(object sender, EventArgs e)
+        {
+            Turtle.Delay = 300;
+            for (int i = 0; i < 36; i++)
+            {
+                Turtle.Forward(250);
+                Turtle.Rotate(170);
+            }
+        }
+
+        private void buttonTriangle_Click(object sender, EventArgs e)
+        {
+            Turtle.Delay = 300;
+            Turtle.PenColor = Color.Red;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int s = 0; s < 22; s++)
+                {
+                    Turtle.Forward(s * 10);
+                    Turtle.Rotate(120);
+                }
+            }
+            
+        }
+    }
+}
